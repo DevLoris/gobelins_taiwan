923d6f602830cf223bee2518c63262724cc6873d
Debalme Mickaël <mickael.debalme@gmail.com>
2021-05-04 16:31:47 +0200

    Debug helpers

---
+++ src/helpers/DebugHelpers.ts
@@ -0,0 +1,34 @@
+/**
+ * Returns true if is localhost
+ * @return boolean
+ */
+export function isLocal(): boolean {
+    return (
+        location.hostname === "localhost"
+        || location.hostname === "127.0.0.1"
+        || location.hostname.includes("192")
+        || location.hostname.includes("172")
+    )
+}
+
+/**
+ * Return true if ?debug=true is in url
+ * @return boolean
+ */
+export function isUrlDebug(): boolean {
+    const urlSearchParams = new URLSearchParams(window.location.search);
+
+    const debugParam = urlSearchParams.get("debug");
+
+    return Boolean(debugParam !== null && debugParam);
+}
+
+/**
+ * Get chapter and step params in url
+ * @return string[]
+ */
+export function getChapterAndStepInUrl(): string[] {
+    const urlSearchParams = new URLSearchParams(window.location.search);
+
+    return [urlSearchParams.get("chapter")?.toUpperCase(), urlSearchParams.get("step")?.toUpperCase()];
+}
